891bb178d99377c5aacc80cd99aa9929e5344cbd
Albany <albany.prince@gmail.com>
2021-03-04 13:12:34 -0500

    reg

--- codesnipe/urls.py
+++ codesnipe/urls.py
@@ -19,6 +19,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('registration.backends.default.urls')),
+
 ]
 
 if settings.DEBUG:
