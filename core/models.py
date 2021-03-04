3baa4d5c3dd18d6e446c080a35639f6a8df11259
Albany <albany.prince@gmail.com>
2021-03-04 13:44:48 -0500

    models, etc

--- core/models.py
+++ core/models.py
@@ -4,3 +4,11 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+class Snippet(models.Model):
+    code = models.TextField()
+    language = models.CharField(max_length=40)
+    title = models.CharField(max_length=40, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} | {self.language}"
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Snippet
 # Create your views here.
+
+def snippet_post(request):
+    snippets = Snippet.objects.all()
+    return render(request, 'core/index.html', {'snippets': snippets})
+
+        
