7d0ce1927c8b38f729cf47e7fed71acb3873b8de
cdiazit5 <cdiazit5@gmail.com>
2021-03-05 13:37:39 -0500

    wrote the view/url/temp for developer profile

--- core/models.py
+++ core/models.py
@@ -8,7 +8,7 @@ class Snippet(models.Model):
     code = models.TextField()
     language = models.CharField(max_length=40)
     title = models.CharField(max_length=40, blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True, null=True, related_name='snippets')
 
     def __str__(self):
         return f"{self.title} | {self.language}"
