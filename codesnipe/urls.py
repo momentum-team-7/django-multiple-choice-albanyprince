7caf0adedd95eb0870f5c9faaa5c2b0191d139f7
cdiazit5 <cdiazit5@gmail.com>
2021-03-06 12:09:17 -0500

    changes to the url

--- codesnipe/urls.py
+++ codesnipe/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('developer/<int:pk>/profile', views.developer_profile, name="developer-profile")
     path('snippet/new', views.add_snippet, name="add-snippet")
+    
 ]
 
 if settings.DEBUG:
