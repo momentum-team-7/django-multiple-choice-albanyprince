5904c26b3dd216d013789784496efd121ce7d20b
cdiazit5 <cdiazit5@gmail.com>
2021-03-10 11:26:30 -0500

    adding some of the styling to search results

--- core/models.py
+++ core/models.py
@@ -18,6 +18,4 @@ class Snippet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True, null=True, related_name='snippets')
 
     def __str__(self):
-        return f"{self.user} | {self.code}"
-
-       
+        return f"{self.user} | {self.title} | {self.language}"
