3ae6c251ecadd237e8c172caf605175b2b89e2c4
Albany <albany.prince@gmail.com>
2021-03-10 16:14:12 -0500

    edit profile work

--- core/forms.py
+++ core/forms.py
@@ -9,6 +9,10 @@ class SnippetForm(forms.ModelForm):
 
 
 class ProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        self.fields['user'].disabled=True
+        self.fields['user'].widget=forms.HiddenInput()
     class Meta:
         model = Profile
         fields = ['user', 'username', 'user_image']
